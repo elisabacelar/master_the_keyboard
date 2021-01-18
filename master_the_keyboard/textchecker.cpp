f8b70c4840d36ae123607b1f98991dfd6b94bdb6
Gabriel <grt.araujo@hotmail.com>
2021-01-18 15:22:08 -0300

    Adding correct text markup.

--- master_the_keyboard/textchecker.cpp
+++ master_the_keyboard/textchecker.cpp
@@ -3,31 +3,45 @@
 QString compareText(QString& referenceString,QString& otherString)
 {
     int stringLength = otherString.length() < referenceString.length() ? otherString.length() : referenceString.length();
-    QString stringBeginning, stringMiddle, stringEnd, stringResult;
+    QString stringBeginning, stringMiddle, stringEnd, stringResult, stringModifier;
     stringResult = "";
     stringEnd = referenceString;
     int stringRemovedLength = 0;
-    int errorStart;
+    int domainStart;
     int it = 0;
-    while(it<stringLength) {
-        if(otherString[it]!=referenceString[it]) {
-            errorStart = it++;
-            while(otherString[it]!=referenceString[it]) {
+    while(it<stringLength)
+    {
+        domainStart = it;
+        if(otherString[it]!=referenceString[it])
+        {
+            stringModifier = "<span style=\"color:#FF0C32;text-decoration:line-through;\">";
+            do
+            {
+                if(it==stringLength)
+                    break;
+                ++it;
+            } while(otherString[it]!=referenceString[it]);
+        }
+        else
+        {
+            stringModifier = "<span style=\"color:#0CFF32;\">";
+            do
+            {
                 if(it==stringLength)
                     break;
                 ++it;
             }
-            stringBeginning = stringEnd.left(it-stringRemovedLength);
-            stringMiddle = stringBeginning.right(stringBeginning.length()-(errorStart-stringRemovedLength));
-            stringBeginning= stringBeginning.left(errorStart-stringRemovedLength);
-            stringRemovedLength += stringBeginning.length() + stringMiddle.length();
-            stringEnd = referenceString.right(referenceString.length()-it);
-            stringResult.append(stringBeginning);
-            stringResult.append("<span style=\"color:#FF0c32;text-decoration:line-through;\">");
-            stringResult.append(stringMiddle);
-            stringResult.append("</span>");
+            while(otherString[it]==referenceString[it]);
         }
-        ++it;
+        stringBeginning = stringEnd.left(it-stringRemovedLength);
+        stringMiddle = stringBeginning.right(stringBeginning.length()-(domainStart-stringRemovedLength));
+        stringBeginning= stringBeginning.left(domainStart-stringRemovedLength);
+        stringRemovedLength += stringBeginning.length() + stringMiddle.length();
+        stringEnd = referenceString.right(referenceString.length()-it);
+        stringResult.append(stringBeginning);
+        stringResult.append(stringModifier);
+        stringResult.append(stringMiddle);
+        stringResult.append("</span>");
     }
     stringResult.append(stringEnd);
     return stringResult;
